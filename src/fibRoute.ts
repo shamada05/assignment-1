7d6f5816b42a588dc3364c66f81adfad5148e874
Salman H <137637055+shamada05@users.noreply.github.com>
2024-08-27 20:32:05 -0400

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,7 +5,7 @@ import fibonacci from "./fib";
 export default (req, res) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num, 10));
+  const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
